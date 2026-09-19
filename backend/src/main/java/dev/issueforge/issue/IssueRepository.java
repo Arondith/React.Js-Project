bfeb9bf36dab21057d560b38e12d251d47546051
Arondite <Aronditee@gmail.com>
2026-09-19 10:59:19 +0800

    feat: add issue repository

---
+++ backend/src/main/java/dev/issueforge/issue/IssueRepository.java
@@ -0,0 +1,8 @@
+package dev.issueforge.issue;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+
+public interface IssueRepository
+        extends JpaRepository<Issue, Long>, JpaSpecificationExecutor<Issue> {
+}
